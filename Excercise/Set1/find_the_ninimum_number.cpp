c0d066d096676cbcc384706218c34e1206fb7585
Nikhil Unni V P <nikhi.unni@gmail.com>
2018-06-04 14:12:30 +0530

    Create find_the_ninimum_number.cpp

---
+++ Excercise/Set1/find_the_ninimum_number.cpp
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n; 
+    scanf("%d",&n);
+    for(int i=1;i<n-1;i++)
+    {
+        printf("min(int, ");
+    }
+    printf("min(int, int)");
+    for(int i=1;i<n-1;i++)
+    {
+        printf(")");
+    }
+    return 0;
+}
